849fe788f6b64b1939c82b0181bd18f2503b0280
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-21 13:33:56 -0600

    exercise 1 done

--- home.js
+++ home.js
@@ -1,16 +1,13 @@
 function validate()
 {
-  let first_pass = document.getElementsByName('fpass')
-  let second_pass = document.getElementById('spass');
+  let first_pass = document.getElementById('fpass').value;
+  let second_pass = document.getElementById('spass').value;
   if (first_pass.length < 8 || second_pass.length < 8)
   {
-    alert ("Password is not long enough"):
+    alert ("Password is not long enough");
   }
   if(first_pass != second_pass)
   {
     alert ("Passwords are not equivalent");
-  }
-  else {
-    alert ("Function called!");
   }
 }
