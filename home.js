c5dd92a09b76363a9eb1eaad0b6fe2392cb5e6ef
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-23 19:04:47 -0600

    slideshow complete

--- home.js
+++ home.js
@@ -12,12 +12,61 @@ function validate()
   }
 }
 
+
+firstImage = "power1.jpg"
+secondImage = "power2.jpg"
+thirdImage = "power3.jpg"
+fourthImage = "power4.jpg"
+fifthImage = "power5.jpg"
+
+
+let x = 0;
 function previous()
 {
-
+  x--;
+  if(Math.abs(x) % 5 == 0)
+  {
+    document.getElementById("slideshow").src = firstImage;
+  }
+  else if(Math.abs(x) % 5 == 1)
+  {
+    document.getElementById("slideshow").src = secondImage;
+  }
+  else if(Math.abs(x) % 5 == 2)
+  {
+    document.getElementById("slideshow").src = thirdImage;
+  }
+  else if( Math.abs(x) % 5 == 3)
+  {
+    document.getElementById("slideshow").src = fourthImage;
+  }
+  else if(Math.abs(x) % 5 == 4)
+  {
+    document.getElementById("slideshow").src = fifthImage;
+  }
 }
 
 function next()
 {
-
+  x++;
+  if(Math.abs(x) % 5 == 0)
+  {
+    document.getElementById("slideshow").src = firstImage;
+  }
+  else if(Math.abs(x) % 5 == 1)
+  {
+    document.getElementById("slideshow").src = secondImage;
+    }
+  else if(Math.abs(x) % 5 == 2)
+  {
+    document.getElementById("slideshow").src = thirdImage;
+  }
+  else if( Math.abs(x) % 5 == 3)
+  {
+    document.getElementById("slideshow").src = fourthImage;
+  }
+  else if(Math.abs(x) % 5 == 4)
+  {
+    document.getElementById("slideshow").src = fifthImage;
+  }
 }
