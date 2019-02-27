c561060415e9195cdb3ed4e7ed222c3a76aa16a6
Cbeeb121 <beaboutclayton@gmail.com>
2019-02-27 15:43:31 -0600

    moving files over

--- home.js
+++ home.js
@@ -19,54 +19,23 @@ thirdImage = "power3.jpg"
 fourthImage = "power4.jpg"
 fifthImage = "power5.jpg"
 
-
 let x = 5;
 function previous()
 {
   x--;
-  if(Math.abs(x) % 5 == 0)
-  {
-    document.getElementById("slideshow").src = firstImage;
-  }
-  else if(Math.abs(x) % 5 == 1)
-  {
-    document.getElementById("slideshow").src = secondImage;
-  }
-  else if(Math.abs(x) % 5 == 2)
-  {
-    document.getElementById("slideshow").src = thirdImage;
-  }
-  else if( Math.abs(x) % 5 == 3)
-  {
-    document.getElementById("slideshow").src = fourthImage;
-  }
-  else if(Math.abs(x) % 5 == 4)
-  {
-    document.getElementById("slideshow").src = fifthImage;
-  }
+  if(Math.abs(x) % 5 == 0){document.getElementById("slideshow").src = firstImage;}
+  else if(Math.abs(x) % 5 == 1){document.getElementById("slideshow").src = secondImage;}
+  else if(Math.abs(x) % 5 == 2){document.getElementById("slideshow").src = thirdImage;}
+  else if( Math.abs(x) % 5 == 3){document.getElementById("slideshow").src = fourthImage;}
+  else if(Math.abs(x) % 5 == 4){document.getElementById("slideshow").src = fifthImage;}
 }
 
 function next()
 {
   x++;
-  if(Math.abs(x) % 5 == 0)
-  {
-    document.getElementById("slideshow").src = firstImage;
-  }
-  else if(Math.abs(x) % 5 == 1)
-  {
-    document.getElementById("slideshow").src = secondImage;
-  }
-  else if(Math.abs(x) % 5 == 2)
-  {
-    document.getElementById("slideshow").src = thirdImage;
-  }
-  else if( Math.abs(x) % 5 == 3)
-  {
-    document.getElementById("slideshow").src = fourthImage;
-  }
-  else if(Math.abs(x) % 5 == 4)
-  {
-    document.getElementById("slideshow").src = fifthImage;
-  }
+  if(Math.abs(x) % 5 == 0){document.getElementById("slideshow").src = firstImage;}
+  else if(Math.abs(x) % 5 == 1){document.getElementById("slideshow").src = secondImage;}
+  else if(Math.abs(x) % 5 == 2){document.getElementById("slideshow").src = thirdImage;}
+  else if( Math.abs(x) % 5 == 3){document.getElementById("slideshow").src = fourthImage;}
+  else if(Math.abs(x) % 5 == 4){document.getElementById("slideshow").src = fifthImage;}
 }
