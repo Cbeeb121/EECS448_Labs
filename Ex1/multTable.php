90b70c44841b46eb7e6cb184eb512544a70cd6b5
Clay Beabout <claybeabout@ku.edu>
2019-03-07 11:07:08 -0600

    public facing index page made

--- Ex1/multTable.php
+++ Ex1/multTable.php
@@ -1,13 +1,17 @@
-<?php
-  echo "<h2> Multiplication Table</h2>";
-  
-  $arr = array(" ");
-
-  for($i; $i<101; $i++)
-  {
-
+  <?php
+  //Inside mult.php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+  echo "<table>";
+  for ($i=1; $i <= 100 ; $i++) {
+    echo "<tr>";
+    for ($j=1; $j <= 100 ; $j++) {
+        echo "<td>". $i * $j . "</td>";
+    }
+    "</tr>";
   }
-
+  "</table>";
+  ?>
 
 
 
